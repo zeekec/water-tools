4aa65bed72c4c7a0e23eed419592a36703640e3a
Erik Zeek <zeekec@gmail.com>
2025-03-03 20:48:02 -0700

    Add a functional form for the main plotting function
    Signed-off-by: Erik Zeek <zeekec@gmail.com>

---
+++ gauge_function.R
@@ -0,0 +1,89 @@
+# graph this year's flow on a river compared to historic record
+# Uses USGS's excelelent dataRetrieval package
+# tutorial here: https://owi.usgs.gov/R/dataRetrieval.html#1
+# Albuquerque gauge to use as example: 08330000
+
+
+library(dataRetrieval)
+library(tidyverse)
+
+get_flow_data <- function(siteNo, startDate = "1895-02-01", endDate = Sys.Date()) {
+    # statistics of interest - this is mean daily discharge
+    pCode <- "00060"
+    statCd <- "00003"
+
+    # retrieve data
+    gauge <- as_tibble(gauge <- readNWISdv(
+        siteNo, pCode, startDate, endDate
+    ))
+    # use pipes to rename flow variable, select ones we need, and
+    # add the year
+    gauge_daily <- gauge %>%
+        rename(flow = X_00060_00003) %>%
+        select(Date, flow, site_no) %>%
+        mutate(year = year(Date))
+
+    # check to remove spurious negative number that showed up that one time
+    # gauge_daily <- filter(gauge_daily, flow>0)
+
+    gauge_daily$yday <- yday(gauge_daily$Date)
+
+    return(gauge_daily)
+}
+
+# guage_function("08330000", "1895-01-01")
+
+guage_function <- function(siteNo,
+                           startDate = "1895-02-01", endDate = Sys.Date(),
+                           plotYear = format(Sys.Date(), "%Y")) {
+    previousYear <- as.character(as.integer(plotYear) - 1)
+
+    # get station metadata
+    gauge_meta <- readNWISsite(siteNo)
+
+    gauge_daily <- get_flow_data(siteNo = siteNo, startDate = startDate, endDate = endDate)
+
+    startDate <- gauge_daily[1, "Date"]$Date
+
+    subtitle <- paste(
+        "Daily flows\nUSGS gauge ",
+        gauge_meta$site_no,
+        "\nData series start date: ", startDate
+    )
+
+    this_year <- filter(gauge_daily, year == plotYear)
+    last_year <- filter(gauge_daily, year == previousYear)
+
+    by_day <- group_by(gauge_daily, yday)
+    gauge_summary <- summarize(by_day,
+        mx = max(flow, na.rm = TRUE),
+        mn = min(flow, na.rm = TRUE),
+        p90 = quantile(flow, .9, na.rm = TRUE),
+        p70 = quantile(flow, .7, na.rm = TRUE),
+        p30 = quantile(flow, .3, na.rm = TRUE),
+        p10 = quantile(flow, .1, na.rm = TRUE),
+        md = median(flow, na.rm = TRUE)
+    )
+    p <- ggplot(gauge_summary, aes(yday)) +
+        theme_bw() +
+        geom_ribbon(aes(ymin = mn, ymax = mx, fill = "min to max")) +
+        geom_ribbon(aes(ymin = p10, ymax = p90, fill = "10th to 90th percentile")) +
+        geom_ribbon(aes(ymin = p30, ymax = p70, fill = "30th to 70th percentile")) +
+        geom_path(aes(x = yday, y = md, color = "median"), linetype = 2) +
+        geom_path(data = this_year, aes(x = yday, y = flow + 1, color = plotYear)) +
+        geom_path(data = last_year, aes(x = yday, y = flow + 1, color = previousYear)) +
+        scale_colour_manual(values = c("black", "red", "blue")) +
+        scale_fill_manual(values = c("lightblue", "lightgreen", "lightgray")) +
+        guides(fill = guide_legend(title = NULL)) +
+        guides(colour = guide_legend(title = NULL)) +
+        # scale_y_log10() +
+        labs(
+            title = gauge_meta$station_nm,
+            x = "Day of the Year",
+            y = expression(paste("Flow (feet"^3, " / second)")),
+            subtitle = subtitle
+        ) +
+        theme(strip.text = element_text(face = "bold", size = 8))
+
+    print(p)
+}
